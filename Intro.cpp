d598750b44263696d709d3cd09a95b477cc9b4e3
Asad Ullah Rasheed <asadullahlahorei@gmail.com>
2026-05-06 16:51:15 +0500

    my introduction

---
+++ Intro.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"********* Welcome to my Program************"<<endl;
+    cout<<"********* My Introduction*************"<<endl;
+    cout<<" My Name "<<endl;
+    cout<<" Asad Ullah "<<endl;
+    cout<<" My age "<<endl;
+    cout<<" 20 "<<endl;
+    cout<<" My Program "<<endl;
+    cout<<" BSSE "<<endl;
+    return 0;
+}
