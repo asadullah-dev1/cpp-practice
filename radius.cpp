1e9194087c238dae13fe944dff75f5a3b6a634ae
Asad Ullah Rasheed <asadullahlahorei@gmail.com>
2026-05-06 17:19:22 +0500

    Finding the radius a program that takes radius of circle from user and calculate the area of circle using the following formula. Area = πr2.

---
+++ radius.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+   float radius;
+    cout<<"Enter the radius of circle "<<endl;
+    cin>>radius;
+      float Area=(3.14)*radius*radius;
+      cout<<"The Area of the Circle is 1"<< Area <<endl;
+        return 0;
+}
