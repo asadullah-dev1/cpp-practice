64dda8533d8bc7177f41312dd789f86ac870f9a9
Asad Ullah Rasheed <asadullahlahorei@gmail.com>
2026-05-12 20:32:05 +0500

    Dmass

---
+++ dmass.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+
+    cout << "Enter two integers: " << endl;
+    cin >> num1 >> num2;
+
+    cout << "The sum is: " << num1 + num2 << endl;
+    cout << "The subtraction is: " << num1 - num2 << endl;
+    cout << "The division is: " << num1 / num2 << endl;
+    cout << "The multiplication is: " << num1 * num2 << endl;
+
+    return 0;
+}
