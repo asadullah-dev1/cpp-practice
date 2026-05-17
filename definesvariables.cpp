ff8575c449dd5cf6269b6f957368d011d859446d
Asad Ullah Rasheed <asadullahlahorei@gmail.com>
2026-05-17 14:45:28 +0500

    Result= Num1 + Num2 / Num3 + Num + Num2++ where Num1, Num2 and Num3 are all integer type variables

---
+++ definesvariables.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num1,num2,num3,num, result;
+ cout<<"Enter num1 num2 num3 num"<<endl;
+ cin>> num1 >> num2 >> num3 >> num;
+    result=num1+num2/num3+num+num2++;
+    cout<<" The result will be " << result << endl;
+    return 0;
+}
