1be6f2e292b414925965f272c1b34a101c00ee5d
Asad Ullah Rasheed <asadullahlahorei@gmail.com>
2026-05-17 14:52:36 +0500

    (m + n) / (p + amount)

---
+++ (m + n)  p + amount).cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int m,n,p,amount,result;
+    cout<<"Enter the value of m,n,p and amount "<<endl;
+    cin>> m >> n >> p >> amount;
+        result=(m+n)/(p+amount);
+        cout<<" the result will be " << result << endl;
+        return 0;
+}
