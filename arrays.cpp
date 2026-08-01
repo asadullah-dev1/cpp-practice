535060b0e3e4ab930f7a72ba121804cd40e98399
Asad <asadullahlahorei@gmail.com>
2026-08-01 14:28:54 +0500

    my first arrays problem

---
+++ arrays.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+int numbers[5]={12,423,42,534,66};
+cout <<" Marks for one is " <<numbers[0]<<endl;
+cout <<" Marks for one is " <<numbers[1]<<endl;
+cout <<" Marks for one is " <<numbers[2]<<endl;
+cout <<" Marks for one is " <<numbers[3]<<endl;
+cout <<" Marks for one is " <<numbers[4]<<endl;
+
+    return 0;
+}
