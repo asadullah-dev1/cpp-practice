cfd025cbed5c7d1743737271d2beeb79517dba7f
Asad <asadullahlahorei@gmail.com>
2026-08-04 16:32:22 +0500

    Sort array in ascending order

---
+++ ascendingarrays.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arrays[5];
+    int temp;
+    for(int i=0; i<5; i++){
+        cout << " Enter Number " << i+1 << endl;
+        cin>>arrays[i]; 
+    }
+        for(int j=0; j<5; j++){
+            
+            for(int z=0; z<arrays[j]; z++){
+                if(j<arrays[j]){
+                    cout << " The ascending order is: " <<endl;
+                }
+               
+        
+        }
+    
+    }
+
+    return 0;
+}
