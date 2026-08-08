cbbb16a69a9115d3870485a3e7ef33cc0cb27fbe
Asad <asadullahdev001@gmail.com>
2026-08-08 15:51:28 +0500

    Create a function add() that takes two integers and returns their sum.

---
+++ fun6.cpp
@@ -0,0 +1,10 @@
+//Create a function add() that takes two integers and returns their sum.
+#include <iostream>
+using namespace std;
+int add(int a, int b){
+    return  a+b;
+}
+int main(){
+    cout << add(72,23) <<endl;;
+    return 0;
+}
