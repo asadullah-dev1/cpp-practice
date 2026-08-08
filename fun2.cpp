53e7781b281435af525025ad0912e0a2e212ac21
Asad <asadullahdev001@gmail.com>
2026-08-08 15:08:20 +0500

    Create a function showNumber() that takes an integer and prints it.

---
+++ fun2.cpp
@@ -0,0 +1,9 @@
+//Create a function showNumber() that takes an integer and prints it.
+#include <iostream> 
+using namespace std;
+void shownumber(int num){
+    cout << num << endl;
+}
+int main () {
+    shownumber(32);
+}
