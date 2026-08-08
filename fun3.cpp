f2c64f12048aa4c605ff6fab5215308f70e1a52c
Asad <asadullahdev001@gmail.com>
2026-08-08 15:22:27 +0500

    Create a function square() that takes an integer and prints its square.

---
+++ fun3.cpp
@@ -0,0 +1,11 @@
+//Create a function square() that takes an integer and prints its square.
+#include <iostream>
+using namespace std;
+void square(int num){
+cout << (num * num) <<endl; 
+}
+int main (){
+square(23);
+square(2);
+return 0;
+}
